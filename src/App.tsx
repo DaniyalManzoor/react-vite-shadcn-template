27eec8bd7509930cf1dfbe71503b630c3feeac21
daniyalmanzoor <daniyalmanzoor28@gmail.com>
2025-05-23 13:03:43 +0500

    init project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello Vite + React! </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+        non, illum porro inventore minima reiciendis architecto voluptate,
+        necessitatibus similique quos ab tempore hic eaque dolorem quibusdam!
+        Accusantium qui vel soluta!
+      </p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
